d9abc141abad01019b568ed78ddf45ce581327cd
yang zhang <tianna1121@gmail.com>
2019-09-26 09:59:17 +0800

    add react-icons for font-awesome component

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,14 +2,15 @@ import React from "react"
 import { Link } from "gatsby"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
+import { FaGulp } from "react-icons/fa"
 
 const IndexPage = () => (
   <Layout>
-    <SEO title="Home" />
+    <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
+    <FaGulp />
   </Layout>
 )
 
